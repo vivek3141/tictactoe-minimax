e27d42fc06c17bb9483b25a594a2ca254cd7ae22
Vivek Verma <vivnps.verma@gmail.com>
2019-04-16 11:49:36 -0700

    Tictactoe Minimax Working

--- game.py
+++ game.py
@@ -9,24 +9,11 @@ class TicTacToe:
         if other:
             self.__dict__ = deepcopy(other.__dict__)
 
-    def start_game(self, human=False):
-        print(self)
-        if human:
-            inp = int(input("Make a move: "))
-            self.move(inp)
-
-    def _move(self, inp):
+    def move(self, inp):
         b = TicTacToe(self)
         b.board[inp // 3][inp % 3] = " " + b.player + " "
         (b.player, b.opponent) = (b.opponent, b.player)
         return b
-
-    def move(self, inp):
-        if self.board[(inp - 1) // 3][(inp - 1) % 3] == "   ":
-            self.board[(inp - 1) // 3][(inp - 1) % 3] = " " + self.player + " "
-        else:
-            return None
-        return self.check_game()
 
     def tie(self):
         t = True
@@ -56,38 +43,6 @@ class TicTacToe:
 
         return False
 
-    def check_game(self):
-        #t = False
-        # for i in self.board:
-        #    for k in i:
-        #        if k == "   ":
-        #            t = True
-        # if not t:
-        #    print("TIE")
-        #    return True
-        for i in range(3):
-            if self.board[i][0] != "   " and \
-                    self.board[i][0] == self.board[i][1] and self.board[i][1] == self.board[i][2]:
-                print(f"{self.board[i][0][1:]}has won!")
-                return True
-
-            if self.board[0][i] != "   " and \
-                    self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i]:
-                print(f"{self.board[0][i][1:]}has won!")
-                return True
-
-        if self.board[0][0] != "   " and \
-                self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2]:
-            print(f"{self.board[0][0][1:]}has won!")
-            return True
-
-        if self.board[0][2] != "   " and \
-                self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][0]:
-            print(f"{self.board[0][2][1:]}has won!")
-            return True
-
-        return False
-
     def __str__(self):
         return "".join([x for i in self.board for x in ["".join([z for k in i for z in [k, "|"]][:-1]) +
                                                         "\n", "-----------" + "\n"]][:-1])
@@ -100,22 +55,25 @@ class TicTacToe:
             if player:
                 return (-1, None)
             else:
-                return (+1, None)
+                return (1, None)
+    
         elif self.tie():
             return (0, None)
+        
         elif player:
             best = (-2, None)
             for i in range(9):
                 if self.board[i // 3][i % 3] == "   ":
-                    value = self._move(i)._minimax(not player)[0]
+                    value = self._move(i)._minimax(not(player))[0]
                     if value > best[0]:
                         best = (value, i)
             return best
+        
         else:
-            best = (+2, None)
+            best = (2, None)
             for i in range(9):
                 if self.board[i // 3][i % 3] == "   ":
-                    value = self._move(i)._minimax(not player)[0]
+                    value = self._move(i)._minimax(not(player))[0]
                     if value < best[0]:
                         best = (value, i)
             return best
@@ -128,6 +86,6 @@ if __name__ == "__main__":
     while True:
         t = t._move(int(input(":")) - 1)
         m = t.best()
-        t = t._move(m)
+        t = t.move(m)
         print(t)
-        #print(t.won())
+        # print(t.won())
