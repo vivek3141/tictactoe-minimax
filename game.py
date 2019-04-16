c31e15d4e6fecf6c6b92d3ae3beacb0a96644c97
Vivek Verma <vivnps.verma@gmail.com>
2019-04-15 22:09:55 -0700

    Adding TicTacToe Game

--- game.py
+++ game.py
@@ -15,7 +15,24 @@ class TicTacToe:
 
     def check_game(self):
         print(self)
-        pass
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] and self.board[i][1] == self.board[i][2]:
+                print(f"{self.board[i][0][1:]}has won!")
+                return True
+
+            if self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i]:
+                print(f"{self.board[0][i][1:]}has won!")
+                return True
+        
+        if self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2]:
+                print(f"{self.board[0][0][1:]}has won!")
+                return True
+        
+        if self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][09]:
+                print(f"{self.board[0][2][1:]}has won!")
+                return True
+        
+        return False
 
     def __str__(self):
         return "".join([x for i in self.board for x in ["".join([z for k in i for z in [k, "|"]][:-1]) +
